21c8061f629c2240dc73e326c722b8e0ecf791eb
adkasyv <kasymbekov01384@gmail.com>
2022-02-01 19:32:28 +0600

    Start, 01/02/2020, 19:32

---
+++ app.js
@@ -0,0 +1,29 @@
+let addMessage = document.querySelector('.message'),
+    addButton  = document.querySelector('.add'),
+    todo       = document.querySelector('.todo');
+
+let todoList = [];
+
+addButton.addEventListener('click', function() {
+    
+    let newTodo = {
+      todo: addMessage.value,
+      checked: false,
+      important: false
+    };
+    
+    todoList.push(newTodo);
+    displayMessages();
+}); 
+
+
+function displayMessages() {
+  
+  todoList.forEach(function(item, i){
+      let displayMessage = `Наше дело: ${addMessage.value}`;
+      console.log('displayMessage: ', displayMessage);
+  });
+}
+
+
+

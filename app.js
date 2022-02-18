c0d549f77c2b2a454c934568ba5f813c087618d7
adkasyv <kasymbekov01384@gmail.com>
2022-02-18 10:10:20 +0600

    Initial commit

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-let addMessage = document.querySelector('.message'),
-    addButton  = document.querySelector('.add'),
-    todo       = document.querySelector('.todo');
+let List = $('#tdlApp ul');
+let Mask = 'tdl_';
 
-let todoList = [];
+function showTasks() {
+  // Узнаём размер хранилища
+  let Storage_size = localStorage.length;
 
-addButton.addEventListener('click', function() {
-    
-    let newTodo = {
-      todo: addMessage.value,
-      checked: false,
-      important: false
-    };
-    
-    todoList.push(newTodo);
-    displayMessages();
-}); 
-
-
-function displayMessages() {
-  
-  todoList.forEach(function(item, i){
-      let displayMessage = `Наше дело: ${addMessage.value}`;
-      console.log('displayMessage: ', displayMessage);
-  });
+  // Если в хранилище что-то есть…
+  if (Storage_size > 0) {
+    // то берём и добавляем это в задачи  
+    for (let i = 0; i < Storage_size; i++) {
+      let key = localStorage.key(i);
+      if (key.indexOf(Mask) == 0) {
+        // и делаем это элементами списка
+        $('<li><li>').addClass('tdItem')
+          .attr('data-itemid', key)
+          .text(localStorage.getItem(key))
+          .appendTo(List);
+      }
+    }
+  }
 }
+
+// Сразу вызываем эту функцию, вдруг в памяти уже остались задачи с прошлого раза
+showTasks();
+
+$('#tdlApp input').on('keydown', function (e) {
+  if (e.keyCode != 13) return;
+  let str = e.target.value;
+  e.target.value = '';
+  // Если в поле ввода было что-то написано — начинаем обрабатывать
+
+  if (str.length > 0) {
+    let number_Id = 0;
+    List.chiilder().each(function (index, el) {
+      let elemet_Id = $(el).attr('data-itemid').slice(4);
+      if (element_Id > number_Id) // ? стоит ли закрыть if??
+        number_Id = element_Id;
+    })
+  })
+});
 
 
 
